7a69a7d3c4479a125b2a4f8049debb96eade191d
Mia Choi <miaminchoe@gmail.com>
2022-04-07 18:20:38 -0400

    added icon, variables to slider values

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@
 let graph = document.getElementById("graph");
 
 // slope
-let m = 0;
+let slope = document.getElementById("slope");
+let slopeSlider = document.getElementById("slope-slider");
+slope.innerHTML = slopeSlider.value;
 
 // y-intercept
-let b = 0;
+let yintercept = document.getElementById("y-intercept");
+let yinterceptSlider = document.getElementById("y-intercept-slider");
+yintercept.innerHTML = yinterceptSlider.value;
 
 // side length of square canvas
 let length = 500;
@@ -16,8 +20,15 @@ let numUnits = 10;
 // spacing
 let spacing = length / numUnits;
 
-function drawGridlines() {
-    let ctx = graph.getContext("2d")
+// draws the gridlines and the axes
+function drawGraph() {
+    let ctx = graph.getContext("2d");
+    drawGridlines(ctx);
+    drawAxes(ctx);
+}
+
+// draws the gridlines
+function drawGridlines(ctx) {
     ctx.strokeStyle = "rgb(230,215,245)";
     ctx.lineWidth = 1;
     ctx.beginPath();
@@ -35,28 +46,44 @@ function drawGridlines() {
     }
 }
 
-function drawAxes() {
-    let ctx = graph.getContext("2d")
+// draws the axes
+function drawAxes(ctx) {
     ctx.strokeStyle = "rgb(40,33,48)";
-    ctx.lineWidth = 3;
-    
-    // x-axis
+    ctx.lineWidth = 2;
     ctx.beginPath();
+
+    // x-axis
     ctx.moveTo(0, length/2);
     ctx.lineTo(length, length/2);
     ctx.stroke();
 
-    // x tick marks
-
     // y-axis
-    ctx.beginPath();
     ctx.moveTo(length/2, 0);
     ctx.lineTo(length/2, length);
+
+    // tick marks
+    for(let i = 0; i <= length; i += spacing) {
+        // x-axis ticks
+        ctx.moveTo(i, length/2 + 3);
+        ctx.lineTo(i, length/2 - 3);
+
+        // y-axis ticks
+        ctx.moveTo(length/2 - 3, i);
+        ctx.lineTo(length/2 + 3,i);
+    }
     ctx.stroke();
-
-    // y tick marks
-
 }
 
-drawGridlines();
-drawAxes();
+
+// updates slope and y-intercept values
+slopeSlider.oninput = function() {
+    slope.innerHTML = this.value;
+}
+
+yinterceptSlider.oninput = function() {
+    yintercept.innerHTML = this.value;
+}
+
+
+
+drawGraph();
